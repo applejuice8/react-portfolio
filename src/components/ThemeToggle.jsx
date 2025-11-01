4c1f29bba9051e40d8469287517d93a19beddac0
Colin <colinleonghongseng@gmail.com>
2025-11-01 15:12:54 +0800

    Completed HTML for theme toggle

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,9 @@
+function ThemeToggle() {
+    return (
+        <button id="theme-toggle" class="pointer">
+            <i class="fas fa-moon"></i>
+        </button>
+    );
+}
+
+export default ThemeToggle;
