a5c54f1bd2f28aaa860442cc54ee8e753f9edd86
Colin <colinleonghongseng@gmail.com>
2025-11-01 18:29:37 +0800

    Separated educationData from education section

--- src/components/Education/EducationSection.jsx
+++ src/components/Education/EducationSection.jsx
@@ -1,27 +1,7 @@
 import EducationItem from './EducationItem';
+import educationData from './educationData';
 
 function EducationSection() {
-    const educationData = [
-        {
-            logos: [
-                { src: 'images/education/Sunway University Dark.png', alt: 'Sunway University' },
-                { src: 'images/education/Lancaster University Dark.png', alt: 'Lancaster University' },
-            ],
-            schoolName: 'Sunway University & Lancaster University',
-            degree: true,
-            programme: 'Bachelor of Science (Honours) in Computer Science',
-            year: 'September 2024 - Current',
-        },
-        {
-            logos: [
-                { src: 'images/education/Sunway College Dark.png', alt: 'Sunway College' },
-            ],
-            schoolName: 'Sunway College',
-            programme: 'Foundation in Science & Technology',
-            year: 'September 2023 - September 2024',
-        },
-    ];
-
     return (
         <section id='education'>
             <h2 className='section-title'>Education</h2>
---
+++ src/components/Education/educationData.js
@@ -0,0 +1,22 @@
+const educationData = [
+    {
+        logos: [
+            { src: 'images/education/Sunway University Dark.png', alt: 'Sunway University' },
+            { src: 'images/education/Lancaster University Dark.png', alt: 'Lancaster University' },
+        ],
+        schoolName: 'Sunway University & Lancaster University',
+        degree: true,
+        programme: 'Bachelor of Science (Honours) in Computer Science',
+        year: 'September 2024 - Current',
+    },
+    {
+        logos: [
+            { src: 'images/education/Sunway College Dark.png', alt: 'Sunway College' },
+        ],
+        schoolName: 'Sunway College',
+        programme: 'Foundation in Science & Technology',
+        year: 'September 2023 - September 2024',
+    },
+];
+
+export default educationData;
