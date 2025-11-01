e39c6b457071c0c65d0e1af98dbe26df22a130b1
Colin <colinleonghongseng@gmail.com>
2025-11-01 18:24:41 +0800

    Added alias path to vite.config.js and updated the paths in skillsData.js

--- src/components/Skills/skillsData.js
+++ src/components/Skills/skillsData.js
@@ -1,51 +1,51 @@
 const skills = {
     'Frontend': [
-        { name: 'HTML', src: '../../assets/frontend/html.svg' },
-        { name: 'CSS', src: '../../assets/frontend/css.svg' },
-        { name: 'JavaScript', src: '../../assets/frontend/javascript.svg' },
-        { name: 'Bootstrap', src: '../../assets/frontend/bootstrap.svg' }
+        { name: 'HTML', src: '@/assets/frontend/html.svg' },
+        { name: 'CSS', src: '@/assets/frontend/css.svg' },
+        { name: 'JavaScript', src: '@/assets/frontend/javascript.svg' },
+        { name: 'Bootstrap', src: '@/assets/frontend/bootstrap.svg' }
     ],
 
     'Backend': [
-        { name: 'Python', src: '../../assets/backend/python.svg' },
-        { name: 'Flask', src: '../../assets/backend/flask.svg' },
-        { name: 'FastAPI', src: '../../assets/backend/fastapi.svg' }
+        { name: 'Python', src: '@/assets/backend/python.svg' },
+        { name: 'Flask', src: '@/assets/backend/flask.svg' },
+        { name: 'FastAPI', src: '@/assets/backend/fastapi.svg' }
     ],
 
     'Database': [
-        { name: 'SQL', src: '../../assets/database/sql.svg' },
-        { name: 'SQLite', src: '../../assets/database/sqlite.svg' },
-        { name: 'Oracle Apex', src: '../../assets/database/oracle apex.svg' }
+        { name: 'SQL', src: '@/assets/database/sql.svg' },
+        { name: 'SQLite', src: '@/assets/database/sqlite.svg' },
+        { name: 'Oracle Apex', src: '@/assets/database/oracle apex.svg' }
     ],
 
     'Machine Learning / Data Science': [
-        { name: 'PyTorch', src: '../../assets/ml/pytorch.svg' },
-        { name: 'Scikit-learn', src: '../../assets/ml/scikit-learn.svg' },
-        { name: 'Pandas', src: '../../assets/ml/pandas.svg' },
-        { name: 'Jupyter', src: '../../assets/ml/jupyter.svg' }
+        { name: 'PyTorch', src: '@/assets/ml/pytorch.svg' },
+        { name: 'Scikit-learn', src: '@/assets/ml/scikit-learn.svg' },
+        { name: 'Pandas', src: '@/assets/ml/pandas.svg' },
+        { name: 'Jupyter', src: '@/assets/ml/jupyter.svg' }
     ],
 
     'Web Scraping & Automation': [
-        { name: 'Selenium', src: '../../assets/scraping/selenium.svg' },
-        { name: 'Playwright', src: '../../assets/scraping/playwright.svg' },
-        { name: 'Beautiful Soup', src: '../../assets/scraping/beautiful soup.svg' }
+        { name: 'Selenium', src: '@/assets/scraping/selenium.svg' },
+        { name: 'Playwright', src: '@/assets/scraping/playwright.svg' },
+        { name: 'Beautiful Soup', src: '@/assets/scraping/beautiful soup.svg' }
     ],
 
     'IDEs': [
-        { name: 'VS Code', src: '../../assets/ides/vs code.svg' },
-        { name: 'Eclipse', src: '../../assets/ides/eclipse.svg' }
+        { name: 'VS Code', src: '@/assets/ides/vs code.svg' },
+        { name: 'Eclipse', src: '@/assets/ides/eclipse.svg' }
     ],
 
     'Version Control': [
-        { name: 'GitHub', src: '../../assets/version control/github.svg' },
-        { name: 'Git', src: '../../assets/version control/git.svg' }
+        { name: 'GitHub', src: '@/assets/version control/github.svg' },
+        { name: 'Git', src: '@/assets/version control/git.svg' }
     ],
 
     'Others': [
-        { name: 'Haskell', src: '../../assets/others/haskell.svg' },
-        { name: 'Linux', src: '../../assets/others/linux.svg' },
-        { name: 'Java', src: '../../assets/others/java.svg' },
-        { name: 'Markdown', src: '../../assets/others/markdown.svg' }
+        { name: 'Haskell', src: '@/assets/others/haskell.svg' },
+        { name: 'Linux', src: '@/assets/others/linux.svg' },
+        { name: 'Java', src: '@/assets/others/java.svg' },
+        { name: 'Markdown', src: '@/assets/others/markdown.svg' }
     ]
 };
 
