2ffd37711bca1c38453abf196e33f942ab729536
Colin <colinleonghongseng@gmail.com>
2025-11-01 15:19:54 +0800

    Completed HTML for about

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+    return (
+        <section id="about">
+            <h2 class="section-title">About Me</h2>
+            <div id="about-content">
+                <p>Hello! I'm <span class="highlight">Colin Leong</span>, a Year 2 <span class="highlight">Computer Science (Hons)</span> student at <span class="highlight">Sunway University</span>.</p>
+                <p>My strongest skill is in <span class="highlight">Python</span>, and I often build automation scripts using <span class="highlight">Selenium</span> to automate repetitive tasks I encounter in daily workflows.</p>
+                <p>I'm passionate about <span class="highlight">Artificial Intelligence</span> and <span class="highlight">Computer Vision</span>. Currently, I'm expanding my knowledge using libraries such as <span class="highlight">Scikit-learn</span> and <span class="highlight">PyTorch</span>.</p>
+                <p>I'm also fascinated by <span class="highlight">Low-level Programming</span>. I don't just want to build scripts that work, I want to understand how they work underneath the hood.</p>
+                <p>I'm constantly pushing myself to learn <span class="highlight">new technologies</span> beyond the classroom.</p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
