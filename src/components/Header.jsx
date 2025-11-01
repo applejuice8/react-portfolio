ca238f1790e8dbe6e7dd87414a9e64c57f1c5b7d
Colin <colinleonghongseng@gmail.com>
2025-11-01 15:16:46 +0800

    Completed HTML for header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import ThemeToggle from "./ThemeToggle";
+
+function Header() {
+    return (
+        <header>
+            <Navbar />
+            <ThemeToggle />
+        </header>
+    );
+}
+
+export default Header;
