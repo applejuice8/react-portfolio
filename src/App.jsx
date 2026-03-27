839229f253c0d76eaa5b522633d0d7fc34e590e9
Colin <colinleonghongseng@gmail.com>
2026-03-27 11:48:53 +0800

    Added section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import Layout from './components/Layout'
+import Section from './components/Section'
 import Home from './components/Home'
 import About from './components/About'
+import Education from './components/Education/Education'
 import Skills from './components/Skills/Skills'
 import Projects from './components/Projects/Projects'
-import Education from './components/Education/Education'
 import Certifications from './components/Certifications/Certifications'
+
+const sections = [Home, About, Education, Skills, Projects, Certifications]
 
 function App() {
   return (
     <Layout>
-      <Home />
-      <About />
-      <Education />
-      <Skills />
-      <Projects />
-      <Certifications />
+      {sections.map((Component) => (
+        <Section key={Component.name} id={Component.name.toLowerCase()}>
+          <Component />
+        </Section>
+      ))}
     </Layout>
   )
 }
